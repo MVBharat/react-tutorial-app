a5cddb7965a00a74e3ece1d17e8a8e618ece3f78
Bharat Mane <bharat.mane@1985.co.in>
2020-06-29 07:31:16 +0530

    Add router and components

---
+++ src/components/add-tutorial.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddTutorial extends Component {
+    render() {
+        return (
+            <div>
+                <p>AddTutorial here</p>
+            </div>
+        )
+    }
+}
+
+export default AddTutorial
---
+++ src/components/tutorials-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TutorialList extends Component {
+    render() {
+        return (
+            <div>
+                <p>TutorialList</p>
+            </div>
+        )
+    }
+}
+
+export default TutorialList
